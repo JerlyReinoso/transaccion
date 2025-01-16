f9ec63f1539aec41b0edeed6e00f5e030d3b5670
Jerly Fernanda Reinoso Namicela <75386318+JerlyReinoso@users.noreply.github.com>
2025-01-16 11:15:38 -0500

    Delete transaccion/transaccion directory

--- transaccion/transaccion/src/main/java/com/espe/transaccion/repositories/TransaccionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.espe.transaccion.repositories;
-
-import com.espe.transaccion.models.entities.Transaccion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransaccionRepository extends JpaRepository<Transaccion, Integer> {
-}
